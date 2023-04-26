9f4a3b484b72f320d25d7d00d223c8ad3e2708df
andreasyia <114986744+andreasyia@users.noreply.github.com>
2023-04-26 12:25:27 +0100

    Add files via upload

---
+++ milestone_2/README.md
@@ -0,0 +1,4 @@
+# Milestone_2
+Milestone 2 contains a list of words and randomly printing the words of the list. Moreover, asks the user for a single letter and validates with if-else statement if the input is of lenght one and is alphabetic.
+
+
---
+++ milestone_2/milestone_2.py
@@ -0,0 +1,17 @@
+import random
+
+word_list = ['banana', 'mango', 'blueberries', 'strawbwrries', 'melon']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input('Enter a single letter:')
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
+
+
