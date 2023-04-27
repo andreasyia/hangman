5f0732421e4d5eaf8e30e352bb0d8640407af5ca
Andreas  Yianni <andreasyianni613@gmail.com>
2023-04-27 17:43:06 +0100

    Hangman Project

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,10 @@
 import random
 
+
 class Hangman:
     '''
     A Hangman Game that asks the user for a letter and checks if it is in the word.
     It starts with a default number of lives and a random word from the word_list.
-
     
     Parameters:
     ----------
